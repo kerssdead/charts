ab8f3b8f6152bcfd0c28a3f68ffb82ed03118da4
kerss <kerss26rus@gmail.com>
2025-08-14 20:03:14 +0300

    Added animations to treemap (#45)

--- src/render/treeRenderer.js
+++ src/render/treeRenderer.js
@@ -5,6 +5,26 @@ class OTreeRenderer extends ORenderer {
     data
 
     /**
+     * @type {DOMRect}
+     */
+    #canvasPosition
+
+    /**
+     * @type {MouseEvent}
+     */
+    #onMouseMoveEvent
+
+    /**
+     * @type {MouseEvent}
+     */
+    #onClickEvent
+
+    /**
+     * @type {boolean}
+     */
+    #isInit
+
+    /**
      * @param {OChart} chart
      * @param {OChartSettings} settings
      * @param {ODynSettings} dynSettings
@@ -15,9 +35,13 @@ class OTreeRenderer extends ORenderer {
         this.data = chart.data
 
         this.data.values.sort((a, b) => b.value - a.value)
+
+        this.#initAnimations()
     }
 
     render() {
+        super.render()
+
         let maxWidth = this.canvas.width,
             maxHeight = this.canvas.height
 
@@ -44,7 +68,10 @@ class OTreeRenderer extends ORenderer {
                     color: item.color,
                     label: item.label,
                     s: item.value / sum * totalSquare,
-                    value: item.value
+                    value: item.value,
+                    id: item.id,
+                    x: x,
+                    y: y
                 }
             ]
 
@@ -55,7 +82,10 @@ class OTreeRenderer extends ORenderer {
                     color: next.color,
                     label: next.label,
                     s: next.value / sum * totalSquare,
-                    value: next.value
+                    value: next.value,
+                    id: next.id,
+                    x: x,
+                    y: y
                 })
 
                 i++
@@ -77,6 +107,8 @@ class OTreeRenderer extends ORenderer {
                         if (!isSingle) {
                             cells[1].w = Math.floor(w)
                             cells[1].h = remainHeight - cells[0].h
+
+                            cells[1].y += cells[0].h
                         }
 
                         break
@@ -95,6 +127,8 @@ class OTreeRenderer extends ORenderer {
                         if (!isSingle) {
                             cells[1].h = Math.floor(h)
                             cells[1].w = remainWidth - cells[0].w
+
+                            cells[1].x += cells[0].w
                         }
 
                         break
@@ -116,6 +150,104 @@ class OTreeRenderer extends ORenderer {
                 }
 
                 ctx.fillStyle = cell.color
+
+                const groupInited = this.#isInit && !this.animations.contains({ id: cell.id }, OAnimationTypes.init)
+
+                if (!groupInited) {
+                    this.animations.add({ id: cell.id },
+                        OAnimationTypes.init,
+                        {
+                            timer: null,
+                            duration: 125 + (this.chart.data.values.indexOf(item) + 1) / this.chart.data.values.length * 175,
+                            before: (item, passed, duration) => {
+                                return passed <= duration
+                            },
+                            body: (passed, duration) => {
+                                if (passed > duration)
+                                    duration = passed
+
+                                const transition = passed / duration
+
+                                const center = {
+                                    x: cell.x + cell.w / 2,
+                                    y: cell.y + cell.h / 2
+                                }
+
+                                const minSize = .7,
+                                    rest = 1 - minSize
+
+                                ctx.translate(center.x - center.x * (minSize + transition * rest),
+                                    center.y - center.y * (minSize + transition * rest))
+                                ctx.scale((minSize + transition * rest), (minSize + transition * rest))
+
+                                let opacity = Math.round(255 * transition).toString(16)
+
+                                if (opacity.length < 2)
+                                    opacity = 0 + opacity
+
+                                ctx.fillStyle = cell.color + opacity
+                            }
+                        })
+                } else {
+                    this.animations.add({ id: cell.id },
+                        OAnimationTypes.mouseover,
+                        {
+                            timer: null,
+                            duration: 140,
+                            before: () => {
+                                return this.#isInCell(cell)
+                            },
+                            body: (passed, duration) => {
+                                if (passed > duration)
+                                    passed = duration
+
+                                const transition = 1 - passed / duration
+
+                                const center = {
+                                    x: cell.x + cell.w / 2,
+                                    y: cell.y + cell.h / 2
+                                }
+
+                                const minSize = .9,
+                                    rest = 1 - minSize
+
+                                ctx.translate(center.x - center.x * (minSize + transition * rest),
+                                    center.y - center.y * (minSize + transition * rest))
+                                ctx.scale((minSize + transition * rest), (minSize + transition * rest))
+
+                                this.animations.reload({ id: cell.id }, OAnimationTypes.mouseleave)
+                            }
+                        })
+
+                    this.animations.add({ id: cell.id },
+                        OAnimationTypes.mouseleave,
+                        {
+                            timer: new Date(2000, 1, 1),
+                            duration: 140,
+                            before: () => {
+                                return !this.#isInCell(cell)
+                            },
+                            body: (passed, duration) => {
+                                if (passed > duration)
+                                    passed = duration
+
+                                const transition = passed / duration
+
+                                const center = {
+                                    x: cell.x + cell.w / 2,
+                                    y: cell.y + cell.h / 2
+                                }
+
+                                const minSize = .9,
+                                    rest = 1 - minSize
+
+                                ctx.translate(center.x - center.x * (minSize + transition * rest),
+                                    center.y - center.y * (minSize + transition * rest))
+                                ctx.scale((minSize + transition * rest), (minSize + transition * rest))
+                            }
+                        })
+                }
+
                 ctx.fillRect(x, y, cell.w, cell.h)
 
                 if (cell.label && OHelper.stringWidth(cell.label) < cell.w) {
@@ -125,6 +257,8 @@ class OTreeRenderer extends ORenderer {
                     ctx.textBaseline = 'middle'
                     ctx.fillText(cell.label, x + cell.w / 2, y + cell.h / 2)
                 }
+
+                ctx.resetTransform()
 
                 if (isVertical)
                     y += cell.h
@@ -148,6 +282,34 @@ class OTreeRenderer extends ORenderer {
 
             isVertical = !isVertical
         }
+
+        requestAnimationFrame(this.render.bind(this))
+
+        this.#isInit = true
+    }
+
+    #initAnimations() {
+        this.#canvasPosition = this.canvas.getBoundingClientRect()
+
+        this.#canvasPosition.x += window.scrollX
+        this.#canvasPosition.y += window.scrollY
+
+        this.canvas.onmousemove = event => this.#onMouseMoveEvent = event
+        this.canvas.onclick = event => this.#onClickEvent = event
+    }
+
+    /**
+     * @param cell {OTreeCell}
+     */
+    #isInCell(cell) {
+        if (!this.#onMouseMoveEvent)
+            return false
+
+        let x = this.#onMouseMoveEvent.clientX - this.#canvasPosition.x + window.scrollX,
+            y = this.#onMouseMoveEvent.clientY - this.#canvasPosition.y + window.scrollY
+
+        return cell.x <= x && x <= cell.x + cell.w
+            && cell.y <= y && y <= cell.y + cell.h
     }
 
     destroy() {
---
+++ src/types/treeCell.js
@@ -0,0 +1,36 @@
+class OTreeCell {
+    /**
+     * @type {number}
+     */
+    s
+
+    /**
+     * @type {number}
+     */
+    value
+
+    /**
+     * @type {number}
+     */
+    x
+
+    /**
+     * @type {number}
+     */
+    y
+
+    /**
+     * @type {string}
+     */
+    color
+
+    /**
+     * @type {string}
+     */
+    label
+
+    /**
+     * @type {string}
+     */
+    id
+}
