7f1a4ef666381bb13e46fe29ef5909586053109a
kerss <kerss26rus@gmail.com>
2025-08-14 19:10:41 +0300

    Added horizontal and vertical treemap (#45)

--- src/render/treeRenderer.js
+++ src/render/treeRenderer.js
@@ -18,8 +18,11 @@ class OTreeRenderer extends ORenderer {
     }
 
     render() {
+        let maxWidth = this.canvas.width,
+            maxHeight = this.canvas.height
+
         let sum = this.data.values.reduce((acc, cur) => acc + cur.value, 0),
-            totalSquare = this.canvas.width * this.canvas.height
+            totalSquare = maxWidth * maxHeight
 
         let x = 0,
             y = 0
@@ -33,14 +36,15 @@ class OTreeRenderer extends ORenderer {
         for (let i = 0; i < this.data.values.length; i++) {
             const item = this.data.values[i]
 
-            const remainWidth = this.canvas.width - minX,
-                remainHeight = this.canvas.height - minY
+            const remainWidth = maxWidth - minX,
+                remainHeight = maxHeight - minY
 
             let cells = [
                 {
                     color: item.color,
                     label: item.label,
-                    s: item.value / sum * totalSquare
+                    s: item.value / sum * totalSquare,
+                    value: item.value
                 }
             ]
 
@@ -50,7 +54,8 @@ class OTreeRenderer extends ORenderer {
                 cells.push({
                     color: next.color,
                     label: next.label,
-                    s: next.value / sum * totalSquare
+                    s: next.value / sum * totalSquare,
+                    value: next.value
                 })
 
                 i++
@@ -59,42 +64,19 @@ class OTreeRenderer extends ORenderer {
             const isSingle = cells.length === 1,
                 isLast = i === this.data.values.length - 1
 
-            const countMultiplier = this.data.values.length / (this.data.values.length / (this.data.values.length / 10))
-
             if (isVertical) {
                 for (let j = 1; j <= remainWidth + i * i; j++) {
-                    const h1 = cells[0].s / j,
-                        h2 = isSingle ? 0 : cells[1].s / j
+                    const w = remainWidth - j,
+                        h1 = cells[0].s / w,
+                        h2 = isSingle ? 0 : cells[1].s / w
 
-                    if (Math.abs(h1 + h2 - remainHeight) <= i * countMultiplier) {
-                    // if (h1 + h2 >= remainHeight) {
-                        cells[0].w = j
-                        cells[0].h = h1
+                    if (h1 + h2 >= remainHeight) {
+                        cells[0].w = Math.floor(w)
+                        cells[0].h = Math.floor(h1)
 
                         if (!isSingle) {
-                            cells[1].w = j
+                            cells[1].w = Math.floor(w)
                             cells[1].h = remainHeight - cells[0].h
-
-                            let ew = j,
-                                eh = Math.abs(h1 + h2 - remainHeight),
-                                es = ew * eh
-
-                            let x = es / (cells[0].h + cells[1].h)
-
-                            // console.log(x)
-
-                            // let ratio = (cells[0].w * cells[0].h) / (cells[1].w * cells[1].h)
-
-                            // console.log(ratio)
-
-                            // let addS1 = es / ratio,
-                            //     addS2 = es - addS1
-
-                            // cells[0].w = (cells[0].s + addS1) / cells[0].h
-                            // cells[1].w = (cells[1].s + addS2) / cells[1].h
-
-                            cells[0].w += x
-                            cells[1].w += x
                         }
 
                         break
@@ -102,38 +84,17 @@ class OTreeRenderer extends ORenderer {
                 }
             } else {
                 for (let j = 1; j <= remainHeight + i * i; j++) {
-                    const w1 = cells[0].s / j,
-                        w2 = isSingle ? 0 : cells[1].s / j
+                    const h = remainHeight - j,
+                        w1 = cells[0].s / h,
+                        w2 = isSingle ? 0 : cells[1].s / h
 
-                    // if (Math.abs(w1 + w2 - remainWidth) <= i * countMultiplier) {
                     if (w1 + w2 >= remainWidth) {
-                        cells[0].h = j
+                        cells[0].h = Math.floor(h)
                         cells[0].w = Math.floor(w1)
 
                         if (!isSingle) {
-                            cells[1].h = j
+                            cells[1].h = Math.floor(h)
                             cells[1].w = remainWidth - cells[0].w
-
-                            let ew = j,
-                                eh = Math.abs(w1 + w2 - remainWidth),
-                                es = ew * eh
-
-                            let x = es / (cells[0].w + cells[1].w)
-
-                            console.log(x)
-
-                            // let ratio = (cells[0].w * cells[0].h) / (cells[1].w * cells[1].h)
-                            //
-                            // console.log(ratio)
-                            //
-                            // let addS1 = es / ratio,
-                            //     addS2 = es - addS1
-                            //
-                            // cells[0].h = (cells[0].s + addS1) / cells[0].w
-                            // cells[1].h = (cells[1].s + addS2) / cells[1].w
-
-                            cells[0].h += x
-                            cells[1].h += x
                         }
 
                         break
@@ -141,35 +102,21 @@ class OTreeRenderer extends ORenderer {
                 }
             }
 
-
-
             for (const cell of cells) {
-                // if (isLast) {
-                //     if (isVertical) {
-                //         cell.w = remainWidth
-                //         if (isSingle)
-                //             cell.h = remainHeight
-                //     } else {
-                //         cell.h = remainHeight
-                //         if (isSingle)
-                //             cell.w = remainWidth
-                //     }
-                // }
+                if (isLast) {
+                    if (isVertical) {
+                        cell.w = remainWidth
+                        if (isSingle)
+                            cell.h = remainHeight
+                    } else {
+                        cell.h = remainHeight
+                        if (isSingle)
+                            cell.w = remainWidth
+                    }
+                }
 
                 ctx.fillStyle = cell.color
                 ctx.fillRect(x, y, cell.w, cell.h)
-
-                // console.log(x, y, cell.w, cell.h, '|', cell.w * cell.h, cell.s, '|', 'diff', Math.abs(cell.w * cell.h - cell.s))
-
-                if (cell.w <= 0 || cell.h <= 0
-                    || isNaN(cell.w) || isNaN(cell.h)) {
-                    console.log(i)
-                    console.log('count:', this.data.values.length)
-                    console.log('isVertical?', isVertical)
-                    if (isLast)
-                        console.log('last')
-                    throw new Error('on render')
-                }
 
                 ctx.fillStyle = '#000000'
                 ctx.font = '14px serif'
@@ -182,6 +129,8 @@ class OTreeRenderer extends ORenderer {
                 else
                     x += cell.w
 
+                totalSquare -= cell.w * cell.h
+                sum -= cell.value
             }
 
             if (isVertical) {
@@ -194,22 +143,6 @@ class OTreeRenderer extends ORenderer {
 
             minX = x
             minY = y
-
-            // console.log(totalSquare, sss)
-
-            if (minX > this.canvas.width
-                || minY > this.canvas.height) {
-                console.log(i)
-                console.log('count:', this.data.values.length)
-                console.log('isVertical?', isVertical)
-                if (isLast)
-                    console.log('last')
-
-                console.log('minX', minX)
-                console.log('minY', minY)
-
-                throw new Error('out of bounds')
-            }
 
             isVertical = !isVertical
         }
