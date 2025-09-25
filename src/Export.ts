e1606f3db810a5b3f0a0d8cb04b2e28e93b4864d
Sergey <kerss26rus@gmail.com>
2025-09-25 18:24:42 +0300

    Fixed displaying opened dropdown in exported png (#231)

--- src/Export.ts
+++ src/Export.ts
@@ -1,75 +1,77 @@
 class Export {
     static asPng(canvas: HTMLCanvasElement, title: string) {
-        const ctx = Helpers.Canvas.getContext(canvas)
+        requestAnimationFrame(() => {
+            const ctx = Helpers.Canvas.getContext(canvas)
 
-        let width = Helper.stringWidth(TextResources.exportPNG) + 16,
-            height = 64
+            let width = Helper.stringWidth(TextResources.exportPNG) + 16,
+                height = 64
 
-        if (width < 50)
-            width = 50
+            if (width < 50)
+                width = 50
 
-        ctx.clearRect(canvas.width - width, 0, width, height)
+            ctx.clearRect(canvas.width - width, 0, width, height)
 
-        let leftEmpty = 0,
-            rightEmpty = 0
+            let leftEmpty = 0,
+                rightEmpty = 0
 
-        const imageData = new Uint32Array(ctx.getImageData(0, 0, canvas.width, canvas.height).data.buffer)
+            const imageData = new Uint32Array(ctx.getImageData(0, 0, canvas.width, canvas.height).data.buffer)
 
-        let isBusy = false
+            let isBusy = false
 
-        for (let i = 0; i < canvas.width; i++) {
-            for (let j = 0; j < canvas.height; j++) {
-                if (Helpers.Canvas.isPixelBusy(imageData[i + j * canvas.width])) {
-                    isBusy = true
-                    break
+            for (let i = 0; i < canvas.width; i++) {
+                for (let j = 0; j < canvas.height; j++) {
+                    if (Helpers.Canvas.isPixelBusy(imageData[i + j * canvas.width])) {
+                        isBusy = true
+                        break
+                    }
                 }
+
+                if (isBusy)
+                    break
+
+                leftEmpty++
             }
 
-            if (isBusy)
-                break
+            isBusy = false
 
-            leftEmpty++
-        }
-
-        isBusy = false
-
-        for (let i = canvas.width; i >= 0; i--) {
-            for (let j = 0; j < canvas.height; j++) {
-                if (Helpers.Canvas.isPixelBusy(imageData[i + j * canvas.width])) {
-                    isBusy = true
-                    break
+            for (let i = canvas.width; i >= 0; i--) {
+                for (let j = 0; j < canvas.height; j++) {
+                    if (Helpers.Canvas.isPixelBusy(imageData[i + j * canvas.width])) {
+                        isBusy = true
+                        break
+                    }
                 }
+
+                if (isBusy)
+                    break
+
+                rightEmpty++
             }
 
-            if (isBusy)
-                break
+            if (leftEmpty > 4)
+                leftEmpty -= 4
+            if (rightEmpty > 4)
+                rightEmpty -= 4
 
-            rightEmpty++
-        }
+            if (leftEmpty > rightEmpty)
+                leftEmpty = rightEmpty
+            if (rightEmpty > leftEmpty)
+                rightEmpty = leftEmpty
 
-        if (leftEmpty > 4)
-            leftEmpty -= 4
-        if (rightEmpty > 4)
-            rightEmpty -= 4
+            let destinationCanvas = document.createElement(Tag.Canvas)
+            destinationCanvas.width = canvas.width - leftEmpty - rightEmpty
+            destinationCanvas.height = canvas.height
 
-        if (leftEmpty > rightEmpty)
-            leftEmpty = rightEmpty
-        if (rightEmpty > leftEmpty)
-            rightEmpty = leftEmpty
+            const destCtx = Helpers.Canvas.getContext(destinationCanvas)
 
-        let destinationCanvas = document.createElement(Tag.Canvas)
-        destinationCanvas.width = canvas.width - leftEmpty - rightEmpty
-        destinationCanvas.height = canvas.height
+            destCtx.fillStyle = Theme.background
+            destCtx.fillRect(0, 0, canvas.width, canvas.height)
 
-        const destCtx = Helpers.Canvas.getContext(destinationCanvas)
+            destCtx.drawImage(canvas, -leftEmpty, 0)
 
-        destCtx.fillStyle = Theme.background
-        destCtx.fillRect(0, 0, canvas.width, canvas.height)
-
-        destCtx.drawImage(canvas, -leftEmpty, 0)
-
-        Export.saveAs((title ?? 'chart') + '.png',
-            destinationCanvas.toDataURL('image/png'))
+            Export.saveAs((title ?? 'chart') + '.png',
+                destinationCanvas.toDataURL('image/png'))
+        })
     }
 
     static asCsv(table: HTMLTableElement, title: string) {
