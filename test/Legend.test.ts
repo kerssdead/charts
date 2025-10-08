6f91935b9263e229c30414b184d72434240e3763
Sergey <kerss26rus@gmail.com>
2025-10-08 18:26:54 +0300

    Added unit-tests for getting offset to center in legend

--- test/Legend.test.ts
+++ test/Legend.test.ts
@@ -2,7 +2,7 @@ import { Value } from '../src/types/base/Value'
 import { Legend } from '../src/Legend'
 
 describe('Calculate legend height', () => {
-    test('Single row', () => {
+    it('Should be single row height', () => {
         const items = [
             {
                 label: 'Sector 1'
@@ -19,7 +19,7 @@ describe('Calculate legend height', () => {
             .toBe(44)
     })
 
-    test('Two rows', () => {
+    it('Should be 2 rows height', () => {
         const items = [
             {
                 label: 'Sector 1'
@@ -40,5 +40,64 @@ describe('Calculate legend height', () => {
 
         expect(Legend.getLegendHeight(items, 500))
             .toBe(70)
+    })
+})
+
+describe('Get offset to center', () => {
+    it('Should close to 100 pixels', () => {
+        const items = [
+            {
+                label: 'Sector 1'
+            } as Value,
+            {
+                label: 'Cell 2'
+            } as Value,
+            {
+                label: 'Sector 3'
+            } as Value
+        ]
+
+        expect(Legend.getOffsetToCenter(items, 500))
+            .toBeCloseTo(100, 0)
+    })
+
+    it('Should be min offset', () => {
+        const items = [
+            {
+                label: 'Sector 1'
+            } as Value,
+            {
+                label: 'Cell 2'
+            } as Value,
+            {
+                label: 'Sector 3'
+            } as Value,
+            {
+                label: 'Cell 4'
+            } as Value,
+            {
+                label: 'Sector 5'
+            } as Value,
+            {
+                label: 'Cell 6'
+            } as Value,
+            {
+                label: 'Sector 7'
+            } as Value
+        ]
+
+        expect(Legend.getOffsetToCenter(items, 200))
+            .toBe(90)
+    })
+
+    it('Should be single centered', () => {
+        const items = [
+            {
+                label: 'Sector 1'
+            } as Value
+        ]
+
+        expect(Legend.getOffsetToCenter(items, 1000))
+            .toBeCloseTo(440, 0)
     })
 })
