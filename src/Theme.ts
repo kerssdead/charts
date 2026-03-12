ae15aa89c650f0693ccb7ca2d947b65f5624d124
kerss <kerss26rus@gmail.com>
2026-03-12 16:26:45 +0300

    Improved theme selection

--- src/Theme.ts
+++ src/Theme.ts
@@ -2,6 +2,11 @@ import * as Helper from 'Helper'
 import ThemeOptions from 'static/ThemeOptions'
 import { Events } from 'static/Enums'
 
+enum ColorTheme {
+    light = 0,
+    dark = 1
+}
+
 class Theme {
     static currentTheme = 0
 
@@ -29,21 +34,19 @@ class Theme {
         if (!Theme.function && isDark)
             Theme.function = isDark
 
-        if (window.matchMedia
-            && window.matchMedia('(prefers-color-scheme: dark)').matches
-            && (!Theme.function || Theme.function()))
-            Theme.setTheme(1)
+        if (!Theme.function || Theme.function())
+            Theme.setTheme(ColorTheme.dark)
         else
-            Theme.setTheme(0)
+            Theme.setTheme(ColorTheme.light)
 
         window.matchMedia('(prefers-color-scheme: dark)')
               .addEventListener(Events.Change, event => {
-                  Theme.setTheme(event.matches && (!Theme.function || Theme.function()) ? 1 : 0)
+                  Theme.setTheme(event.matches && (!Theme.function || Theme.function()) ? ColorTheme.dark : ColorTheme.light)
                   callback()
               })
     }
 
-    static setTheme(index: number) {
+    static setTheme(index: ColorTheme) {
         Theme.currentTheme = index
 
         Theme.text = ThemeOptions.colors[index]
@@ -51,7 +54,7 @@ class Theme {
         Theme.line = ThemeOptions.lines[index]
         Theme.lineAxis = ThemeOptions.lineAxes[index]
         Theme.lineActive = ThemeOptions.lineActives[index]
-        Theme.dropdownItemHoverColor = Helper.adjustColor(Theme.background, index == 0 ? -50 : 50)
+        Theme.dropdownItemHoverColor = Helper.adjustColor(Theme.background, index == ColorTheme.light ? -50 : 50)
         Theme.dropdownBorder = ThemeOptions.dropdownBorders[index]
         Theme.canvasBackground = ThemeOptions.canvasBackgrounds[index]
         Theme.canvasBackgroundInt = Number(`0xff${ Theme.canvasBackground.slice(1) }`)
